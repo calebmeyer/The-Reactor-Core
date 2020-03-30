34d6e6dbd7026214dc64abf633d3ef3cec1ea9d5
Caleb Meyer <kiaulen@gmail.com>
2020-03-29 20:04:01 -0500

    Move config from package.json to eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = {
     dir: {
+        input: './source',
         // look in source/templates instead of _include for layouts and other includes
         includes: 'static',
         layouts: 'templates'
