b1d4a9a060b53bd114aa44ab2ef0f239b3cbf9f6
Caleb Meyer <Kiaulen@gmail.com>
2023-10-24 18:34:32 +0000

    Upgrade to 2.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
-module.exports = {
+module.exports = function (eleventyConfig) {
+  return {
     dir: {
-        input: './source',
-        // look in source/templates instead of _include for layouts and other includes
-        includes: 'static',
-        layouts: 'templates'
+      input: "./source",
+      // look in source/templates instead of _include for layouts and other includes
+      includes: "static",
+      layouts: "templates",
     },
-    templateFormats: ['md', 'html', 'liquid', 'png']
+    templateFormats: ["md", "html", "liquid", "png"],
+  };
 };
